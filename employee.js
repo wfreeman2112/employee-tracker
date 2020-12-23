d39ea042e2a7abe7d4f0bc8feae3ae521d4494bc
Wendi Freeman <wfreeman2112@gmail.com>
2020-12-23 14:52:09 -0600

    connected port

--- employee.js
+++ employee.js
@@ -1,10 +1,14 @@
 //establish dependencies
 
+var express = require("express");
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 require("console.table");
 
 //building server
+var app = express();
+var PORT = process.env.PORT || 3000;
+
 var connection = mysql.createConnection({
     host: "localhost",
     PORT:  process.env.PORT || 3000,
@@ -12,3 +16,13 @@ var connection = mysql.createConnection({
     password: "password1",
   database: "employees_DB",
 });
+
+// connection.connect(function (err) {
+//     if (err) throw err;
+//     runSearch();
+//   });
+
+  //starts server to begin listening
+    app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
